6038f43325a53685798106e2830b5895c8bcc974
prog_evil <fl_cute@163.com>
2021-07-16 23:48:20 +0800

    test

--- src/main/java/com/hdc/seckill/controller/DemoController.java
+++ src/main/java/com/hdc/seckill/controller/DemoController.java
@@ -1,14 +1,29 @@
 package com.hdc.seckill.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @ClassName DemoController
- * @Description TODO
+ * @Description 测试
  * @Author prog_evil
  * @Date 2021/7/16 11:28 下午
  * @Version 1.0
  **/
 @Controller
+@RequestMapping("/demo")
 public class DemoController {
+    /*
+     * @Author ProG_Evil
+     * @Description //测试页面跳转
+     * @Date 11:42 下午 2021/7/16
+     * @Param [model]
+     * @return java.lang.String
+     **/
+    @RequestMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("name","hdc");
+        return "hello";
+    }
 }
