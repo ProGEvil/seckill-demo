3a0d7d663c9e9112db1ba95770c5bad89a68930f
海东晨 <prog_evil@ProG-Evil.local>
2021-07-16 23:30:02 +0800

    Initial commit

---
+++ src/main/java/com/hdc/seckill/SecKillDemoApplication.java
@@ -0,0 +1,15 @@
+package com.hdc.seckill;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.hdc.seckill.pojo")
+public class SecKillDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecKillDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/hdc/seckill/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.hdc.seckill.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @ClassName DemoController
+ * @Description TODO
+ * @Author prog_evil
+ * @Date 2021/7/16 11:28 下午
+ * @Version 1.0
+ **/
+@Controller
+public class DemoController {
+}
